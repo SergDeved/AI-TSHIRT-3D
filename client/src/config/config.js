01a6967bc7d72199371f5016ec7246ddd3a77fb4
Egioooo <134957932+SergDeved@users.noreply.github.com>
2023-06-07 01:53:00 +0200

    Expose the app in LAN

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,9 +2,9 @@ const config = {
   development: {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
-  // production: {
-  //   backendUrl: "https://notinproduction/api/v1/dalle",
-  // },
+  production: {
+    backendUrl: "http://192.168.0.100/api/v1/dalle",
+  },
 };
 
 export default config;
--- server/index.js
+++ server/index.js
@@ -8,12 +8,16 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "50mb" }))
+app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/dalle", dalleRoutes);
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: "Hello from DALL.E" })
-})
+    res.status(200).json({ message: "Hello from DALL.E" });
+});
 
-app.listen(8080, () => console.log('Server has started on port 8080'))
+const localhostPort = 8080;
+const localIpAddress = 'localhost';
+const networkIpAddress = '192.168.0.100';
+app.listen(localhostPort, localIpAddress, () => console.log(`Server has started at ${localIpAddress}:${localhostPort}`));
+app.listen(localhostPort, networkIpAddress, () => console.log(`Server has started at ${networkIpAddress}:${localhostPort}`));
